a85ccfa3a1d1a54bf511465e9d6097443bc43abf
GuilhermeD9 <sgsouza.173@gmail.com>
2026-02-18 13:43:49 -0300

    feature: Arquivos iniciais do projeto

---
+++ src/main/java/dev/guilherme/desafio_sgt/DesafioSgtApplication.java
@@ -0,0 +1,13 @@
+package dev.guilherme.desafio_sgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSgtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/guilherme/desafio_sgt/DesafioSgtApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guilherme.desafio_sgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
